038d0b92b3ce4d64021c12e53613d0fe9608034f
Joseph Kniskern <kniskernjoseph@yahoo.com>
2020-10-03 21:17:18 -0400

    another commit

---
+++ src/components/counters.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class Counters extends Component {
+
+  state = { 
+    counters: [
+      { id: 1, value: 3},
+      { id: 2, value: 0},
+      { id: 3, value: 0},
+      { id: 4, value: 0}
+    ]
+   }
+
+  handleDelete = (counterId) => {
+
+  }
+
+  render() { 
+    return ( 
+      <div>
+        {this.state.counters.map(counter => 
+        (<Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} selected={true}/>))}
+      </div>
+     );
+  }
+}
+ 
+export default Counters;
